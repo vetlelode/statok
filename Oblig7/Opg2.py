abd55feab74898b185295039289700ac83b9ba55
Vetle Lode <vetlelode@gmail.com>
2020-04-17 03:00:08 +0200

    started working on the next assignment

---
+++ Oblig7/Opg2.py
@@ -0,0 +1,3 @@
+import numpy as np
+
+A = np.array([48, 33, 37, 13, 37, 23, 31, 37, 22, 11])
---
+++ Oblig7/Opg7.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+
+data = np.array([[100, 200, 300, 400, 500], [240, 480, 670, 820, 1040]])
+print(data[0, :], data[1, :])
+b_hat, a_hat, r_value, p_value, std_err = stats.linregress(
+    data[0, :], data[1, :])
+print("Y = {} + {}x\nStandardfeil: {}".format(a_hat, b_hat, std_err))
